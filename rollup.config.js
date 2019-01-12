a64388e0659b8536ca65003226fbb7ceb6da7aa9
aelbore <aelbore.sg@gmail.com>
2019-01-12 15:54:36 +0800

    add rollup plugin, update dependencies version

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,45 @@
+import alias from 'rollup-plugin-strict-alias'
 import vue from 'rollup-plugin-vue';
 import buble from 'rollup-plugin-buble';
 import bundleSize from 'rollup-plugin-filesize';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs'
 import pkg from './package.json';
+
+import { terser } from "rollup-plugin-terser";
 
 const external = Object.keys(pkg.dependencies);
 const isProduction = true;
 
 const plugins = [
+  alias({
+    vue: 'node_modules/vue/dist/vue.js'
+  }),
   resolve(),
   bundleSize(),
   vue({
     template: {
-      isProduction,
-      compilerOptions: { preserveWhitespace: false }
+      isProduction
     },
-    css: true,
+    css: true
   }),
-  buble()
+  commonjs(),
+  buble(),
+  terser()
 ];
 
 export default {
-  external,
+  external: [],
   plugins,
-  input: [
+  input: [ 
     'src/main.js'
   ],
   output: [
     {
-      globals: { 
-        vue: 'Vue' 
-      },
+      globals: {},
       dir: 'dist',
       format: 'umd',
       sourcemap: true
     }
-  ] ,
-  experimentalCodeSplitting: true,
+  ]
 };
